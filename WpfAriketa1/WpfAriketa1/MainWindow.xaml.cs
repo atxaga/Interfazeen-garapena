abb3b9a9fdf8e9fbae339bebe347dc7226aa8cf4
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-10 09:01:41 +0200

    interfazeen ariketa1 eta 2

---
+++ WpfAriketa1/WpfAriketa1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAriketa1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(zenb1.Text);
+            int b = int.Parse(zenb2.Text);
+            int c = int.Parse(zenb3.Text);
+            int d = int.Parse(zenb4.Text);
+
+            int resultado = (a + 2 * b + 3 * c + 4 * d) / 4;
+
+            emaitza.Text = resultado.ToString();
+        }
+
+        private void BtnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            zenb1.Clear();
+            zenb2.Clear();
+            zenb3.Clear();
+            zenb4.Clear();
+            emaitza.Clear();
+        }
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfAriketa2/WpfAriketa2/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAriketa2
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string[] frases = new string[5];
+        private int fraseIndex = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FraseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (fraseIndex < 5)
+            {
+                frases[fraseIndex] = frasetexbox.Text;
+                ((Button)sender).IsEnabled = false;
+                frasetexbox.Clear();
+                fraseIndex++;
+
+                if (fraseIndex < 5)
+                {
+                    var nextButton = (Button)FindName($"frase{fraseIndex + 1}");
+                    if (nextButton != null)
+                        nextButton.IsEnabled = true;
+                }
+                else
+                {
+                    unir.IsEnabled = true;
+                    frasetexbox.IsEnabled = false;
+                }
+            }
+        }
+
+        private void UnirButton_Click(object sender, RoutedEventArgs e)
+        {
+            frasetexbox.Text = string.Join(" ", frases);
+            unir.IsEnabled = false;
+        }
+
+        private void LimpiarButton_Click(object sender, RoutedEventArgs e)
+        {
+            fraseIndex = 0;
+            for (int i = 0; i < frases.Length; i++)
+                frases[i] = string.Empty;
+
+            frasetexbox.Clear();
+            frasetexbox.IsEnabled = true;
+            frase1.IsEnabled = true;
+            frase2.IsEnabled = false;
+            frase3.IsEnabled = false;
+            frase4.IsEnabled = false;
+            frase5.IsEnabled = false;
+            unir.IsEnabled = false;
+        }
+
+        private void SalirButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
