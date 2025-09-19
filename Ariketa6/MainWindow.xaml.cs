b5b3419c40a8de504c7f5d269e9f791406ada6ac
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-19 10:50:41 +0200

    ariketa 7 bukatzen

--- Ariketa6/MainWindow.xaml.cs
+++ Ariketa6/MainWindow.xaml.cs
@@ -19,6 +19,49 @@ namespace Ariketa6
         public MainWindow()
         {
             InitializeComponent();
+            this.KeyDown += MainWindow_KeyDown;
+
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+               if (textbox1.Text != "")
+                {
+
+                    var textua = textbox1.Text;
+                    textbox2.Text = textua;
+                    textbox1.Text = "";
+
+                } else if (textbox2.Text != "")
+                {
+                    
+                    var textua = textbox2.Text;
+                    textbox3.Text = textua;
+                    textbox2.Text = "";
+
+                } else if (textbox3.Text != "")
+                {
+
+                    var textua = textbox3.Text;
+                    textbox1.Text = textua;
+                    textbox3.Text = "";
+                }
+                   
+            }
+        }
+
+        private void Button_limpiar_Click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text = "";
+            textbox2.Text = "";
+            textbox3.Text = "";
+        }
+
+        private void Button_salir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
---
+++ Ariketa7/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ariketa7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int count = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_number_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                string number = button.Content.ToString();
+                if (count == 0)
+                {
+                    numbrebox.Text = number;
+                    count++;
+                }
+                else
+                {
+                    numbrebox.Text += number;
+                    count++;
+                }
+
+            }
+                   
+        }
+    }
+}
