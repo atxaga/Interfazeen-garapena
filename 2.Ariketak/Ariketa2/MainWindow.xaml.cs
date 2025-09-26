cfccef2bc775c0e6265f36539065f00e618f330f
iker <iachagaro22wg@ikzubirimanteo.com>
2025-09-26 23:11:52 +0200

    2.Ariketak bukatuta

--- 2.Ariketak/Ariketa2/MainWindow.xaml.cs
+++ 2.Ariketak/Ariketa2/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Ariketa2
 {
@@ -19,6 +11,51 @@ namespace Ariketa2
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Añadir_Click(object sender, RoutedEventArgs e)
+        {
+            string nuevoAmigo = txtAmigoNuevo.Text.Trim();
+            if (!string.IsNullOrEmpty(nuevoAmigo) && !lstAmigos.Items.Contains(nuevoAmigo))
+            {
+                lstAmigos.Items.Add(nuevoAmigo);
+                txtAmigoNuevo.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Introduzca datos para poder añadirlos");
+            }
+        }
+
+        private void Button_Amigos_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (lstAmigos.SelectedItem != null)
+            {
+                txtAmigoSeleccionado.Text = lstAmigos.SelectedItem.ToString();
+            }
+        }
+
+        private void Button_Eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstAmigos.SelectedItem != null)
+            {
+                lstAmigos.Items.Remove(lstAmigos.SelectedItem);
+                txtAmigoSeleccionado.Clear();
+            } else
+            {
+                MessageBox.Show("Introduzca datos para poder añadirlos");
+            }
+        }
+
+        private void Button_BorrarLista_Click(object sender, RoutedEventArgs e)
+        {
+            lstAmigos.Items.Clear();
+            txtAmigoSeleccionado.Clear();
+        }
+
+        private void Button_Salir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
